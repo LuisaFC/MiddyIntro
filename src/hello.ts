ed631e38932f1ecb5e28bec36d9675003f810e53
LuisaFC <luisabsb@gmail.com>
2025-08-14 11:06:21 -0300

    Middleware Response Serializer

--- src/hello.ts
+++ src/hello.ts
@@ -3,10 +3,9 @@ import { makeHandler } from './middy/makeHandler'
 export const handler = makeHandler(async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: JSON.stringify({
-                nameSent: event?.body?.name,
-            }),
-        }),
+        body: {
+            firstName: event?.body?.firstName,
+            lastName: event?.body?.lastName,
+        }
     }
 })
--- src/middy/makeHandler.ts
+++ src/middy/makeHandler.ts
@@ -1,10 +1,20 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpResponseSerializer from '@middy/http-response-serializer'
 
 type Handler = (event: any) => Promise<any>;
 
 export function makeHandler(handler: Handler) {
-    return middy()
-    .use(httpJsonBodyParser({disableContentTypeError: true}))
-    .handler(handler);
+  return middy()
+  .use(httpJsonBodyParser({disableContentTypeError: true}))
+  .use(httpResponseSerializer({
+    defaultContentType: 'application/json',
+    serializers: [
+      {
+        regex: /^application\/json$/,
+        serializer: ({body}) => JSON.stringify(body)
+      }
+    ]
+  }))
+  .handler(handler);
 }
